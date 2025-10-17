707f22c984c74751beac2153df875f4816807722
YJ Soon <yjsoon@tinkertanker.com>
2025-10-17 13:27:01 +0800

    Initial Commit

---
+++ ChatGPTRapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPTRapper
+//
+//  Created by YJ on 17/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
